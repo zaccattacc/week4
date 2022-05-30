c784a5f0ec017d438d4fd9bb37a1f6b723240a0f
Zacc Elisha <zacheause@gmail.com>
2022-05-30 17:56:04 +0100

    First commit.

--- frontend_assignment/pages/api/greet.ts
+++ frontend_assignment/pages/api/greet.ts
@@ -9,15 +9,21 @@ import type { NextApiRequest, NextApiResponse } from "next"
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     const { greeting, nullifierHash, solidityProof } = JSON.parse(req.body)
 
-    const contract = new Contract("0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512", Greeter.abi)
+    
     const provider = new providers.JsonRpcProvider("http://localhost:8545")
+    const contract = new Contract("0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512", Greeter.abi, provider)
 
     const contractOwner = contract.connect(provider.getSigner())
 
     try {
-        await contractOwner.greet(utils.formatBytes32String(greeting), nullifierHash, solidityProof)
-
-        res.status(200).end()
+        const tx = await contractOwner.greet(utils.formatBytes32String(greeting), nullifierHash, solidityProof)
+        const receipt = await tx.wait()
+        console.log(receipt.events)
+        if (receipt.events.length > 0) {
+            const resultGreeting = receipt.events[0].args.greeting;
+            res.status(200).send(utils.parseBytes32String(resultGreeting))
+        }
+        
     } catch (error: any) {
         const { message } = JSON.parse(error.body).error
         const reason = message.substring(message.indexOf("'") + 1, message.lastIndexOf("'"))
--- frontend_assignment/pages/index.tsx
+++ frontend_assignment/pages/index.tsx
@@ -5,9 +5,27 @@ import { providers } from "ethers"
 import Head from "next/head"
 import React from "react"
 import styles from "../styles/Home.module.css"
+import { useForm, Controller, SubmitHandler } from "react-hook-form"
+import { Input, Stack, FormControl, Button } from "@chakra-ui/react"
+import * as yup from "yup"
+import { yupResolver } from "@hookform/resolvers/yup"
+import { ChakraProvider } from '@chakra-ui/react'
+
+interface IFormInput {
+    name: string;
+    age: number;
+    address: string;
+}
+
+const schema = yup.object({
+    name: yup.string().required(),
+    age: yup.number().positive().integer().required(),
+    address: yup.string().required()
+}).required();
 
 export default function Home() {
     const [logs, setLogs] = React.useState("Connect your wallet and greet!")
+    const [sentGreet, setSentGreet] = React.useState('Your Greeting will appear here!')
 
     async function greet() {
         setLogs("Creating your Semaphore identity...")
@@ -50,14 +68,75 @@ export default function Home() {
             })
         })
 
+
         if (response.status === 500) {
             const errorMessage = await response.text()
 
             setLogs(errorMessage)
         } else {
             setLogs("Your anonymous greeting is onchain :)")
+            const greetMessage = await response.text()
+            setSentGreet(greetMessage)
         }
     }
+
+    const { control, handleSubmit, formState: { errors } } = useForm<IFormInput>({
+        mode: "onBlur",
+        resolver: yupResolver(schema),
+    });
+    const onSubmit: SubmitHandler<IFormInput> = data => console.log(data);
+
+    function Form(){
+        return (
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <Stack spacing={4}>
+                    <FormControl>
+                        <Controller
+                            name="name"
+                            control={control}
+                            defaultValue=""
+                            render={({ field }) => 
+                                <div>
+                                    <Input {...field} placeholder="Name"/>
+                                    <p>{errors.name?.message}</p>
+                                </div>
+                            }
+                        />
+                    </FormControl>
+                    <FormControl>
+                        <Controller 
+                            name="age"
+                            control={control}
+                            defaultValue={0}
+                            render={({ field }) => 
+                                <div>
+                                    <Input {...field} placeholder="Age"/>      
+                                    <p>{errors.age?.message}</p>
+                                </div>
+                            }
+                        />
+                        
+                    </FormControl>
+                    
+                    <Controller
+                        name="address"
+                        control={control}
+                        defaultValue=""
+                        render={({ field }) =>
+                            <div> 
+                                <Input {...field} placeholder="Address"/>
+                                <p>{errors.address?.message}</p>
+                            </div>
+                        }
+                    />
+                    <Button type="submit">Submit</Button>
+                </Stack>
+                
+            </form>
+        )
+    }
+
+
 
     return (
         <div className={styles.container}>
@@ -77,6 +156,15 @@ export default function Home() {
                 <div onClick={() => greet()} className={styles.button}>
                     Greet
                 </div>
+
+                <div style={{padding: "20px 0px"}}>
+                    <ChakraProvider>
+                        <Form />
+                        <Input placeholder={sentGreet} isReadOnly={true} />
+                    </ChakraProvider>
+                </div>
+
+                
             </main>
         </div>
     )
